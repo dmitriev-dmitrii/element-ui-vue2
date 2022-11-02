773c639acd77c0cb7da4a80a46ad8d8a25857357
Dmitriev-Dmitrii <dmitriev.dmitrii@mail.com>
2022-11-02 22:56:19 +0300

    save progress

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,24 @@
 <template>
   <div >
 
+<el-form >
 
-<div style="margin-top: 15px; max-width:600px" >
+  <el-form-item size="mini"
+                error="выы"
+  >
 
-  <el-input placeholder="Please input" v-model="organizations.searchQuery" @input="organizationsSearchRequest" class="input-with-select">
-    <el-select v-model="organizations.searchBy" slot="prepend" placeholder="Select" class="organizations__search-select">
-  
+    <el-checkbox v-model="organizations.selectAll"> Выбрать все организации
+      <el-tooltip class="item" effect="dark" content="Top Left prompts info" placement="top-start">
+        <i class="el-icon-info" />
+    </el-tooltip> </el-checkbox>
+  </el-form-item>
+  <el-form-item
+  >
+
+
+  <el-input :disabled="organizations.selectAll" placeholder="Поиск организаций" v-model.trim="organizations.searchQuery" @input="organizationsSearchRequest" class="organizations__search-input" >
+    <el-select :disabled="organizations.selectAll" v-model="organizations.searchBy" slot="prepend" placeholder="Select" >
+
   <el-option v-for="option in organizations.searchByOptions"  :key="option.value" :label="option.label" :value="option.value"></el-option>
 
   </el-select>
@@ -14,38 +26,34 @@
   </el-input>
 
 
+<el-card class="organizations__search-result" v-show="organizations.searchQuery">
 
-<el-card>
-
-  <div  v-show="organizations.loading">
+  <div  v-if="organizations.loading">
     <i class="el-icon-loading"></i> Поиск...
   </div>
 
-  <div v-if="!organizations.loading && organizations.searchResultArr.length > 0">
-  
-    <el-button  v-for="item in organizations.searchResultArr" :key="item.id" @click="organizationsAddHandle(item.id)"> 
-      <span> {{item.name}}</span> <span>  {{item.id }} {{item.checked}}</span> 
-    </el-button>
+  <div v-else>
+      <p  v-show="!organizations.searchResultArr.length">
+        Нет данных
+    </p>
+
+    <el-tag :type="item.checked ? '' : 'info' "  disable-transitions  v-for="item in organizations.searchResultArr" :key="item.id" @click="organizationsCheckHandle(item)">
+       <span> <i class="el-icon-check"  :class="{ hidden:!item.checked }"/> {{item.name}}</span> <span> ID: {{item.id }}</span>
+    </el-tag>
+
   </div >
 
-  <p v-else>
-    Нет данных
-  </p>
 
- 
 </el-card>
-{{
-  organizations.selectedArr
-}}
 
-  <div v-for="item in organizations.selectedArr" :key="item.id" >
-    <el-tag @close="abc(item)" closable  class="text item"    :disable-transitions="false"> 
-      <span> {{item.name}}</span> <span>  {{item.id }} {{item.checked}}</span> 
-    </el-tag>
-  </div>
+    <div class="organizations__selected" v-show="!organizations.selectAll">
+      <el-tag v-for="item in organizations.selectedArr"  :key="item.id" @close="organizationsCheckHandle(item)" closable  class="text item"    :disable-transitions="false">
+        <span> {{item.name}}</span> <span> ID:  {{item.id }} </span>
+      </el-tag>
+    </div>
 
-
-</div>
+  </el-form-item>
+</el-form>
 
 
   </div>
@@ -57,37 +65,35 @@
       return {
         organizations: {
                 loading: false,
-                selectedArr: [], // список выбранных организаций 
-                searchResultArr: [], 
+                selectedArr: [{id: 66633, checked:true,external_id: null, name: "ООО Компания ОмскСантехМетал"},{ "id": 32, "external_id": null, "name": "ПАО АвтоСантехТяжБанк", "checked": true }], // список выбранных организаций
+                searchResultArr: [],
                 searchBy:'name',
-                searchQuery:'',
+                searchQuery:'ss',
                 searchByOptions: [{value:'name' , label :"Название"}, {value:'id' , label :"ID"} ],
                 selectAll: false,
             },
-
       };
     },
 
     methods: {
      async organizationsSearchRequest () {
-      
 
       try {
+
         this.organizations.loading = true;
         this.organizations.searchResultArr = []
-        let res = await new Promise((resolve) => {setTimeout(() => {resolve([{id: 32, external_id: null, name: "ПАО АвтоСантехТяжБанк"},{id: 33, external_id: null, name: "ООО Компания ОмскСантехМетал"}]);}, 1000);});
-        
-        
+        let res = await new Promise((resolve) => {setTimeout(() => {resolve([{id: 32, external_id: null, name: "ПАО АвтоСантехТяжБанк"},{id: 33, external_id: null, name: "ООО Компания ОмскСантехМетал"},{id: 55, external_id: null, name: "fhdfjj fddg gf gd"}]);}, 100);});
+        const selectedIdArr = this.organizations.selectedArr.map((item)=>{
+          return item.id
+        })
+        this.organizations.searchResultArr = res.map((item)=>{
 
-          this.organizations.searchResultArr = res.map((item)=>{
-            
-            item.checked = this.selectedArr.some(element => item.id === element.id)
+            item.checked =  selectedIdArr.includes(item.id)
 
             return item
 
-          }) 
-          
-        
+          })
+
 
       }catch (err){
           console.log(err)
@@ -97,32 +103,76 @@
 
       },
 
-      handleClose(item) {
-        console.log(item);
-      },
+      organizationsCheckHandle(item){
 
-      organizationsAddHandle(id){
-       
-        const targetIndex = this.organizations.searchResultArr.findIndex((item)=>{
-             return item.id === id
+if(!Object.keys(item).length){
+  return;
+}
+
+        const selectedArrTargetIndex = this.organizations.selectedArr.findIndex((selectedItem)=>{
+          return selectedItem.id === item.id
         })
 
-        if ( targetIndex != -1  ) {
-          this.organizations.selectedArr = [...this.organizations.selectedArr, ...this.organizations.searchResultArr.splice(targetIndex,1) ]
-        }
-      
+        const searchArrTargetIndex = this.organizations.searchResultArr.findIndex((selectedItem)=>{
+          return selectedItem.id === item.id
+        })
+
+
+if (!item.checked) {
+  this.organizations.searchResultArr[searchArrTargetIndex].checked = true;
+  this.organizations.selectedArr.unshift(this.organizations.searchResultArr[searchArrTargetIndex])
+  return;
+}
+
+   if(selectedArrTargetIndex > -1 ){
+     this.organizations.selectedArr.splice(selectedArrTargetIndex,1)
+   }
+   if(searchArrTargetIndex > -1 ){
+          this.organizations.searchResultArr[searchArrTargetIndex].checked = false
+   }
       }
     },
     mounted() {
-
+      this.organizationsSearchRequest ()
     }
-  
+
   };
 </script>
 
 
-<style >
-.organizations__search-select{
-  min-width: 120px;
+<style  lang="scss">
+.organizations
+{
+
+  &__search-result{
+    margin-bottom: 1.5rem;
+    & .el-card__body{
+      padding: 0 ;
+    }
+    & .el-tag{
+      width: 100%;
+        display: flex;
+        justify-content: space-between;
+
+    }
+    .el-icon-check.hidden {
+      opacity: 0;
+    }
+  }
+
+  &__selected {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  &__search-input{
+
+    & .el-select{
+      min-width: 120px;
+    }
+
+  }
+
 }
+
 </style>
