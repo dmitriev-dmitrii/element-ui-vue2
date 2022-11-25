4a96c879af629fdabcdd72db9e5517274c314d78
Dmitriev-Dmitrii <56238391+dmitriev-dmitrii@users.noreply.github.com>
2022-11-25 15:27:10 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-
     <TagsInput />
   </div>
 </template>
 
 <script>
+import _get from "lodash/get";
 import TagsInput from './components/TagsInput.vue'
 
 export default {
   name: 'App',
   components: {
     TagsInput
+  },
+  mounted  (){
+    console.log(_get(this,'hui' ));
   }
 }
 </script>
